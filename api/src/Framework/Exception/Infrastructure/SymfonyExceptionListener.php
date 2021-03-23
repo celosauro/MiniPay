24209743a09c788c3aaf8979029ad7583b33c949
Marcelo Santos <marcelo.santos@superlogica.com>
2021-03-23 19:23:59 -0300

    Adiciona error para sumfonylistener

--- api/src/Framework/Exception/Infrastructure/SymfonyExceptionListener.php
+++ api/src/Framework/Exception/Infrastructure/SymfonyExceptionListener.php
@@ -4,126 +4,62 @@ declare(strict_types=1);
 
 namespace MiniPay\Framework\Exception\Infrastructure;
 
-use InvalidArgumentException;
 use MiniPay\Framework\Exception\Domain\DebugHandler;
 use MiniPay\Framework\Exception\Domain\ErrorHandler;
 use MiniPay\Framework\Exception\Domain\GenericErrorHandler;
 use MiniPay\Framework\Exception\Domain\LcobucciErrorHandler;
 use MiniPay\Framework\Exception\Domain\NotEncodableValueErrorHandler;
 use MiniPay\Framework\Exception\Domain\NotFoundHttpErrorHandler;
+use MiniPay\Framework\Exception\Domain\NotNormalizableValueErrorHandler;
 use MiniPay\Framework\Exception\Domain\SymfonyMessengerErroUnpacker;
 use MiniPay\Framework\Exception\Domain\ValidationFailedErrorHandler;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 
-use function implode;
-use function in_array;
-use function sprintf;
-
 class SymfonyExceptionListener
 {
-    /**
-     * In DEVELOPMENT mode:
-     *  - handle exceptions: NOT ALL - only exceptions supported by configured handlers will be handled. Others
-     *  exceptions will be ignored by the listener in order to allow Symfony to display its nice default Error Page.
-     *  - attach debug information into HTTP Responses: YES
-     */
-    public const MODE_DEVELOPMENT = 'development';
+    private bool $debug;
 
-    /**
-     * In DEBUG mode:
-     *  - handle exceptions: ALL
-     *  - attach debug information into HTTP Responses: YES
-     */
-    public const MODE_DEBUG = 'debug';
-
-    /**
-     * In PRODUCTION mode:
-     *  - handle exceptions: all
-     *  - attach debug information into HTTP Responses: YES
-     */
-    public const MODE_PRODUCTION = 'production';
-
-    private const MODES = [
-        self::MODE_DEBUG,
-        self::MODE_DEVELOPMENT,
-        self::MODE_PRODUCTION,
-    ];
-
-    private string $mode;
-
-    public function __construct(string $mode)
+    public function __construct(bool $debug)
     {
-        if (in_array($mode, self::MODES, true) === false) {
-            throw new InvalidArgumentException(sprintf(
-                'The given mode (%s) is unknown. Supported modes: %s.',
-                $mode,
-                implode(', ', self::MODES)
-            ));
-        }
-
-        $this->mode = $mode;
+        $this->debug = $debug;
     }
 
     public function onKernelException(ExceptionEvent $event): void
     {
         $exception = $event->getThrowable();
-        $handlers = $this->configuredHandlers();
+
+        $handlers = [
+            new SymfonyMessengerErroUnpacker(new LcobucciErrorHandler()),
+            new SymfonyMessengerErroUnpacker(new ValidationFailedErrorHandler()),
+            new SymfonyMessengerErroUnpacker(new NotEncodableValueErrorHandler()),
+            new SymfonyMessengerErroUnpacker(new NotNormalizableValueErrorHandler()),
+            new SymfonyMessengerErroUnpacker(new NotFoundHttpErrorHandler()),
+        ];
 
         foreach ($handlers as $handler) {
-            if ($handler->canHandleWith($exception) === false) {
+            if (! $handler->canHandleWith($exception)) {
                 continue;
             }
+
+            $this->attachDebugHandlerIfDebugIsEnabled($handler);
 
             $event->setResponse($handler->handle($exception));
 
             return;
         }
+
+        $genericException = new GenericErrorHandler();
+        $response = $genericException->handle($exception);
+
+        $event->setResponse($response);
     }
 
-    /**
-     * @return array<ErrorHandler>
-     */
-    private function configuredHandlers(): array
+    private function attachDebugHandlerIfDebugIsEnabled(ErrorHandler &$errorHandler): void
     {
-        $handlers = [
-            new SymfonyMessengerErroUnpacker(new LcobucciErrorHandler()),
-            new SymfonyMessengerErroUnpacker(new ValidationFailedErrorHandler()),
-            new SymfonyMessengerErroUnpacker(new NotEncodableValueErrorHandler()),
-            new SymfonyMessengerErroUnpacker(new NotFoundHttpErrorHandler()),
-        ];
-
-        if ($this->shouldHandleExceptionsNotSupportedByConfiguredHandlers()) {
-            $handlers[] = new GenericErrorHandler();
+        if (! $this->debug) {
+            return;
         }
 
-        if ($this->shouldAttachDebugInformationToResponse() === false) {
-            return $handlers;
-        }
-
-        foreach ($handlers as $index => $handler) {
-            $handlers[$index] = new DebugHandler($handler);
-        }
-
-        return $handlers;
-    }
-
-    private function shouldHandleExceptionsNotSupportedByConfiguredHandlers(): bool
-    {
-        $modesThatHandleUnsupportedExceptions = [
-            self::MODE_DEBUG,
-            self::MODE_PRODUCTION,
-        ];
-
-        return in_array($this->mode, $modesThatHandleUnsupportedExceptions, true);
-    }
-
-    private function shouldAttachDebugInformationToResponse(): bool
-    {
-        $modesThatAttachesDebugInformationIntoResponse = [
-            self::MODE_DEVELOPMENT,
-            self::MODE_DEBUG,
-        ];
-
-        return in_array($this->mode, $modesThatAttachesDebugInformationIntoResponse, true);
+        $errorHandler = new DebugHandler($errorHandler);
     }
 }
--- api/tests/unit/Framework/Exception/Infrastructure/SymfonyExceptionListenerTest.php
+++ api/tests/unit/Framework/Exception/Infrastructure/SymfonyExceptionListenerTest.php
@@ -5,218 +5,61 @@ declare(strict_types=1);
 namespace MiniPay\Tests\Framework\Exception\Infrastructure;
 
 use Exception;
-use InvalidArgumentException;
 use MiniPay\Framework\Exception\Infrastructure\SymfonyExceptionListener;
 use MiniPay\Tests\Framework\Exception\Domain\UserNotFound;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Throwable;
 
-use function assert;
-use function json_decode;
+use function json_encode;
 
 class SymfonyExceptionListenerTest extends TestCase
 {
     /**
-     * @dataProvider allSupportedModes
+     * @param string[] $expectedResponse
+     *
+     * @dataProvider additionProvider
      */
-    public function testGivenModeIsASupportedMode(string $mode): void
-    {
-        $listener = new SymfonyExceptionListener(
-            $mode
-        );
-
-        $this->assertInstanceOf(SymfonyExceptionListener::class, $listener);
-    }
-
-    public function testShouldThrowExceptionWhenGivenModeDoesntExists(): void
-    {
-        $this->expectException(InvalidArgumentException::class);
-        $listener = new SymfonyExceptionListener(
-            'nonexistent mode'
-        );
-    }
-
-    /**
-     * @dataProvider modesThatHandleSupportedExceptions
-     */
-    public function testGivenModeShouldCreateResponseForSupportedException(string $mode): void
-    {
-        $listener = new SymfonyExceptionListener(
-            $mode
-        );
-        $event = $this->aExceptionEventFor(new UserNotFound('User was not found'));
-
-        $listener->onKernelException($event);
-
-        $this->assertInstanceOf(Response::class, $event->getResponse());
-    }
-
-    /**
-     * @dataProvider modesThatHandleUnsupportedExceptions
-     */
-    public function testGivenModeShouldCreateResponseForUnsupportedException(string $mode): void
-    {
-        $listener = new SymfonyExceptionListener(
-            $mode
-        );
-        $event = $this->aExceptionEventFor(new Exception('There is no handler for this exception class'));
-
-        $listener->onKernelException($event);
-
-        $this->assertInstanceOf(Response::class, $event->getResponse());
-    }
-
-    /**
-     * @dataProvider modesThatDoesntHandleUnsupportedExceptions
-     */
-    public function testGivenModeShouldNotCreateResponseForUnsupportedException(string $mode): void
-    {
-        $listener = new SymfonyExceptionListener(
-            $mode
-        );
-        $event = $this->aExceptionEventFor(new Exception('There is no handler for this exception class'));
-
-        $listener->onKernelException($event);
-
-        $this->assertNull($event->getResponse());
-    }
-
-    /**
-     * @dataProvider modesThatAppendDebugInformation
-     */
-    public function testGivenModeShouldAppendDebugInformationInRespose(string $mode): void
-    {
-        $listener = new SymfonyExceptionListener(
-            $mode
-        );
-        $event = $this->aExceptionEventFor(new UserNotFound('User was not found'));
-
-        $listener->onKernelException($event);
-
-        $response = $event->getResponse();
-        assert($response instanceof Response);
-        $responseAsArray = json_decode((string) $response->getContent(), true);
-        $this->assertArrayHasKey('debug', $responseAsArray);
-        $this->assertNotEmpty($responseAsArray['debug']);
-    }
-
-    /**
-     * @dataProvider modesThatDoesntAppendDebugInformation
-     */
-    public function testGivenModeShouldNotAppendDebugInformationInRespose(string $mode): void
-    {
-        $listener = new SymfonyExceptionListener(
-            $mode
-        );
-        $event = $this->aExceptionEventFor(new UserNotFound('User was not found'));
-
-        $listener->onKernelException($event);
-
-        $response = $event->getResponse();
-        assert($response instanceof Response);
-        $responseAsArray = json_decode((string) $response->getContent(), true);
-        $this->assertArrayNotHasKey('debug', $responseAsArray);
-    }
-
-    /**
-     * @return array<mixed>
-     */
-    public function allSupportedModes(): array
-    {
-        return [
-            'MODE_DEVELOPMENT' => [
-                SymfonyExceptionListener::MODE_DEVELOPMENT,
-            ],
-            'MODE_DEBUG' => [
-                SymfonyExceptionListener::MODE_DEBUG,
-            ],
-            'MODE_PRODUCTION' => [
-                SymfonyExceptionListener::MODE_PRODUCTION,
-            ],
-        ];
-    }
-
-    /**
-     * @return array<mixed>
-     */
-    public function modesThatHandleSupportedExceptions(): array
-    {
-        return [
-            'MODE_DEVELOPMENT' => [
-                SymfonyExceptionListener::MODE_DEVELOPMENT,
-            ],
-            'MODE_DEBUG' => [
-                SymfonyExceptionListener::MODE_DEBUG,
-            ],
-            'MODE_PRODUCTION' => [
-                SymfonyExceptionListener::MODE_PRODUCTION,
-            ],
-        ];
-    }
-
-    /**
-     * @return array<mixed>
-     */
-    public function modesThatHandleUnsupportedExceptions(): array
-    {
-        return [
-            'MODE_DEBUG' => [
-                SymfonyExceptionListener::MODE_DEBUG,
-            ],
-            'MODE_PRODUCTION' => [
-                SymfonyExceptionListener::MODE_PRODUCTION,
-            ],
-        ];
-    }
-
-    /**
-     * @return array<mixed>
-     */
-    public function modesThatDoesntHandleUnsupportedExceptions(): array
-    {
-        return [
-            'MODE_DEVELOPMENT' => [
-                SymfonyExceptionListener::MODE_DEVELOPMENT,
-            ],
-        ];
-    }
-
-    /**
-     * @return array<mixed>
-     */
-    public function modesThatAppendDebugInformation(): array
-    {
-        return [
-            'MODE_DEVELOPMENT' => [
-                SymfonyExceptionListener::MODE_DEVELOPMENT,
-            ],
-            'MODE_DEBUG' => [
-                SymfonyExceptionListener::MODE_DEBUG,
-            ],
-        ];
-    }
-
-    /**
-     * @return array<mixed>
-     */
-    public function modesThatDoesntAppendDebugInformation(): array
-    {
-        return [
-            'MODE_PRODUCTION' => [
-                SymfonyExceptionListener::MODE_PRODUCTION,
-            ],
-        ];
-    }
-
-    private function aExceptionEventFor(Throwable $throwable): ExceptionEvent
+    public function testOnKernelException(Throwable $exception, array $expectedResponse): void
     {
         $kernel = $this->getMockBuilder(HttpKernelInterface::class)->getMock();
+
         $request = Request::create('/');
 
-        return new ExceptionEvent($kernel, $request, HttpKernelInterface::MASTER_REQUEST, $throwable);
+        $event = new ExceptionEvent($kernel, $request, HttpKernelInterface::MASTER_REQUEST, $exception);
+
+        $listener = new SymfonyExceptionListener(false);
+        $listener->onKernelException($event);
+
+        $response = $event->getResponse() ?? null;
+        $responseContent = $response ?
+            (string) $response->getContent()
+            : '';
+        $responseCode = $response
+            ? $response->getStatusCode()
+            : 0;
+
+        $this->assertJsonStringEqualsJsonString(
+            json_encode($expectedResponse) ?: '',
+            $responseContent
+        );
+        $this->assertEquals($exception->getCode(), $responseCode);
+    }
+
+    /** @return array<mixed> */
+    public function additionProvider(): array
+    {
+        return [
+            'when is a generic exception ' => [
+                new Exception('Internal Server Error', 500),
+                ['detail' => 'Internal Server Error'],
+            ],
+            'when is a lcobucci exception' => [
+                new UserNotFound('User was not found', 404),
+                ['detail' => 'User was not found', 'title' => null, 'type' => null],
+            ],
+        ];
     }
 }
